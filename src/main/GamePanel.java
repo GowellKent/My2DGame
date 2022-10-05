aa6de16d768b9d8efc4eaec2e796a9e34766da9b
GowellKent <Gowellkent1@gmail.com>
2022-10-05 11:22:56 +0700

    gameloop and FPS counter

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -15,17 +15,14 @@ public class GamePanel extends JPanel implements Runnable{
     //screen settings
     final int oriTileSize = 16; //16x16 tile size
     final int scale = 3;
-    
     final int tileSize = oriTileSize * scale;
     final int maxScreenCol = 16;
     final int maxScreenRow = 12;
-    
     final int screenWidth = tileSize * maxScreenCol; //768px
     final int screenHeight = tileSize * maxScreenRow; //576px
 
     //setting FPS variable
     int FPS = 60;
-
     KeyHandler keyH = new KeyHandler();
     Thread gameThread;
 
@@ -51,41 +48,76 @@ public class GamePanel extends JPanel implements Runnable{
         gameThread.start();
     }
 
+    // THIS IS SLEEP GAME LOOP METHOD
+
+//    @Override
+//    public void run() {
+//
+//        //if the game runs on 30fps = loops 30times per second
+//        //if the game runs on 60fps = loops 60times per second
+//
+////        getting current system time in nano seconds
+////        long currentTime = System.nanoTime();
+//
+//        double drawInterval = 1000000000/FPS;
+//        double nextDrawTime = System.nanoTime() + drawInterval;
+//
+//        while (gameThread != null){
+//            //this loop is used for :
+//            //1. UPDATE : such as updating character positions
+//            update();
+//            //2. DRAW : drawing the screen with updated information
+//            repaint();
+//
+//            try {
+//                double remainingTime = nextDrawTime - System.nanoTime();
+//                remainingTime = remainingTime / 1000000;
+//
+//                if(remainingTime < 0){
+//                    remainingTime = 0;
+//                }
+//
+//                Thread.sleep((long) remainingTime); //sleeping thread for remaining time to limit loops
+//
+//                nextDrawTime += drawInterval;
+//
+//            } catch (InterruptedException e) {
+//                throw new RuntimeException(e);
+//            }
+//
+//        }
+//    }
+
+    //THIS IS DELTA GAME LOOP METHOD
     @Override
-    public void run() {
-
-        //if the game runs on 30fps = loops 30times per second
-        //if the game runs on 60fps = loops 60times per second
-
-//        getting current system time in nano seconds
-//        long currentTime = System.nanoTime();
+    public void run(){
 
         double drawInterval = 1000000000/FPS;
-        double nextDrawTime = System.nanoTime() + drawInterval;
+        double delta = 0;
+        long lastTime = System.nanoTime();
+        long currentTime;
+        long timer = 0;
+        int drawCount = 0;
 
-        while (gameThread != null){
-            //this loop is used for :
-            //1. UPDATE : such as updating character positions
-            update();
-            //2. DRAW : drawing the screen with updated information
-            repaint();
+        while(gameThread != null){
 
-            try {
-                double remainingTime = nextDrawTime - System.nanoTime();
-                remainingTime = remainingTime / 1000000;
+            currentTime = System.nanoTime();
+            delta += (currentTime - lastTime)/ drawInterval;
+            timer += (currentTime - lastTime);
+            lastTime = currentTime;
 
-                if(remainingTime < 0){
-                    remainingTime = 0;
-                }
-
-                Thread.sleep((long) remainingTime);
-
-                nextDrawTime += drawInterval;
-
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
+            if (delta >= 1){
+                update();
+                repaint();
+                delta--;
+                drawCount++;
             }
 
+            if (timer >= 1000000000){
+                System.out.println(drawCount + "FPS");
+                drawCount = 0;
+                timer = 0;
+            }
         }
     }
 
