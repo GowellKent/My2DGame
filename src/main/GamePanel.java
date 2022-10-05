02ddcb2c2c24341f16bf55b04c37bb7e12adae79
GowellKent <Gowellkent1@gmail.com>
2022-10-05 10:32:15 +0700

    Adding Key Handlers

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -4,15 +4,14 @@
  */
 package main;
 
-import java.awt.Color;
-import java.awt.Dimension;
-import javax.swing.JPanel;
+import java.awt.*;
+import javax.swing.*;
 
 /**
  *
  * @author gowell
  */
-public class GamePanel extends JPanel{
+public class GamePanel extends JPanel implements Runnable{
     //screen settings
     final int oriTileSize = 16; //16x16 tile size
     final int scale = 3;
@@ -23,7 +22,9 @@ public class GamePanel extends JPanel{
     
     final int screenWidth = tileSize * maxScreenCol; //768px
     final int screenHeight = tileSize * maxScreenRow; //576px
-    
+
+    Thread gameThread;
+
     public GamePanel(){
         
         this.setPreferredSize(new Dimension(screenWidth, screenHeight));
@@ -31,5 +32,40 @@ public class GamePanel extends JPanel{
         this.setDoubleBuffered(true);
         
     }
-    
+
+    public void startGameThread(){
+
+        gameThread = new Thread(this);
+        gameThread.start();
+    }
+
+    @Override
+    public void run() {
+
+        //if the game runs on 30fps = loops 30times per second
+        //if the game runs on 60fps = loops 60times per second
+        while (gameThread != null){
+//            System.out.println("The Game Loop is Running!");
+            //this loop is used for :
+            //1. UPDATE : such as updating character positions
+            update();
+            //2. DRAW : drawing the screen with updated information
+            repaint();
+
+        }
+    }
+
+    public void update() {
+
+    }
+
+    public void paintComponent(Graphics g){
+
+        super.paintComponent(g);
+        Graphics2D g2 = (Graphics2D)g;
+
+        g2.setColor(Color.white);
+        g2.fillRect(100, 100, tileSize, tileSize);
+        g2.dispose();
+    }
 }
--- src/main/Main.java
+++ src/main/Main.java
@@ -17,12 +17,16 @@ public class Main {
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         window.setResizable(false);
         window.setTitle("2D Adventure");
-        
+
+        KeyHandler keyH = new KeyHandler();
         GamePanel gamePanel = new GamePanel();
+
         window.add(gamePanel);
         window.pack();
         
         window.setLocationRelativeTo(null);
         window.setVisible(true);
+
+        gamePanel.startGameThread();
    }
 }
